a2003821e09bc129f10456e7682e5ecf3eb79691
ceciliaz030 <zhangyixin319@gmail.com>
2024-01-09 03:56:44 +0800

    verify_pipeline

--- bin-powdr/src/main.rs
+++ bin-powdr/src/main.rs
@@ -1,4 +1,9 @@
+use powdr::number::GoldilocksField;
 use powdr::riscv::{compile_rust, CoProcessors};
+use powdr::pipeline::{
+    pipeline::Pipeline,
+    test_util::verify_pipeline
+};
 use std::path::{Path, PathBuf};
 
 fn main() {
@@ -13,4 +18,7 @@ fn main() {
     )
     .ok_or_else(|| vec!["could not compile rust".to_string()])
     .unwrap();
+    let pipeline: Pipeline<GoldilocksField> =
+        Pipeline::default().from_asm_string(asm_contents, Some(PathBuf::from(asm_file_path)));
+    verify_pipeline(pipeline, Vec::new(), Vec::new());
 }
